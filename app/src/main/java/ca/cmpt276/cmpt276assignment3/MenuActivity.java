bd5d51faff611d469949294dc6d57e152c6a5342
Dongwei Han <handongwei917@hotmail.com>
2022-10-21 02:49:57 -0700

    remove useless comments and imports.

--- app/src/main/java/ca/cmpt276/cmpt276assignment3/MenuActivity.java
+++ app/src/main/java/ca/cmpt276/cmpt276assignment3/MenuActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
